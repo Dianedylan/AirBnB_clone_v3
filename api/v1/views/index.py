545599fab5ad468fb16a1771ce3d685107a19b45
Dianedylan <dianagatwiriikiamba@gmail.com>
2023-01-07 16:55:07 +0300

    correction on stats to retrieve no. of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,9 +19,16 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    return (jsonify({"amenities": storage.count(Amenity),
-                     "cities": storage.count(City),
-                     "places": storage.count(Place),
-                     "reviews": storage.count(Review),
-                     "states": storage.count(State),
-                     "users": storage.count(User)}))
+    """retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        }
+    return jsonify(stats)
+
+if __name__ == "__main__":
+    pass
